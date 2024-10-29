f6f378817ba1f87aa7ea6d545fcf89469d604eb1
ShaoKuy <iqtanawad@gmail.com>
2024-10-29 22:31:36 +0700

    Add input system

--- Assets/Script/Zoovivals/JoyStick.cs
+++ Assets/Script/Zoovivals/JoyStick.cs
@@ -1,18 +1,55 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class JoyStick : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Movement Setting")] 
+    [SerializeField] private float moveSpeed = 5f;
+    
+    private Rigidbody2D rb;
+    private Vector2 moveInput;
+    private bool facingRight = true;
+    
+    private void Awake()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
-
-    // Update is called once per frame
-    void Update()
+    
+    public void OnMove(InputValue value)
     {
-        
+        moveInput = value.Get<Vector2>();
+    }
+    
+    private void FixedUpdate()
+    {
+        MovePlayer();
+        FlipSprite();
+    }
+    
+    private void MovePlayer()
+    {
+        Vector2 movement = new Vector2(moveInput.x * moveSpeed, moveInput.y * moveSpeed);
+        rb.velocity = movement;
+    }
+    
+    private void FlipSprite()
+    {
+        if (moveInput.x > 0 && !facingRight)
+        {
+            Flip();
+        }
+        else if (moveInput.x < 0 && facingRight)
+        {
+            Flip();
+        }
+    }
+    
+    private void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 scale = transform.localScale;
+        scale.x *= -1;
+        transform.localScale = scale;
     }
 }
